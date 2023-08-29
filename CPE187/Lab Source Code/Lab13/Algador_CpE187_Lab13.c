c2f52e7d938da7880409c48a6a3aab65dc481aab
Kim <89118577+krspykim07@users.noreply.github.com>
2023-08-29 15:37:56 -0700

    Add files via upload

---
+++ CPE187/Lab Source Code/Lab13/Algador_CpE187_Lab13.c
@@ -0,0 +1,89 @@
+// Lab13.c
+// Runs on LM4F120 or TM4C123
+// Use SysTick interrupts to implement a 4-key digital piano
+// edX Lab 13 
+// Daniel Valvano, Jonathan Valvano
+// December 29, 2014
+// Port B bits 3-0 have the 4-bit DAC
+// Port E bits 3-0 have 4 piano keys
+
+#include "..//tm4c123gh6pm.h"
+#include "Sound.h"
+#include "Piano.h"
+#include "TExaS.h"
+
+// basic functions defined at end of startup.s
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void delay(unsigned long msec);
+unsigned long input,previous;
+int main(void){ // Real Lab13 
+	// for the real board grader to work 
+	// you must connect PD3 to your DAC output
+  TExaS_Init(SW_PIN_PE3210, DAC_PIN_PB3210,ScopeOn); // activate grader and set system clock to 80 MHz
+// PortE used for piano keys, PortB used for DAC        
+	SYSCTL_RCGC2_R |= 0x1A;
+  Sound_Init(); // initialize SysTick timer and DAC
+  Piano_Init();
+  EnableInterrupts();  // enable after all initialization are done
+	previous = Piano_In();
+  while(1){                
+// input from keys to select tone
+input = Piano_In(); 
+    if(input&&(previous==0)) {     	
+			  if(input==0x01){  //Piano key 0 pressed	
+            Sound_Tone(4778);   	//80000000/(16*523.251)=9556/2=4778
+				}	else { 
+						if(input==0x02) { //		Piano key 1 pressed			
+							  Sound_Tone(4257); //80000000/(16*587.330)=8513/2=4257 
+						} else {
+								if(input==0x04) { //Piano key 2 pressed
+									Sound_Tone(3792); //80000000/(16*659.255)=7584/2=3792
+								} else {  
+										if(input==0x08) { ////Piano key 3 pressed							 
+											Sound_Tone(3189); //80000000/(16*783.991)=6378/2=3189
+										}
+								} 
+							} 
+					} 
+			}
+  	if(input&&previous) { 
+			if(input==0x01) {  
+				Sound_Tone(4778);
+			} else { 
+					if(input==0x02){ 
+						Sound_Tone(4257);
+					} else {
+							if(input==0x04){ 
+								Sound_Tone(3792); 
+							} else {
+									if(input==0x08){ 					 
+										Sound_Tone(3189);
+									} else{
+											Sound_Off();
+										} 
+								} 
+						} 
+				}
+		}
+	if(previous&&(input==0)){ // just released     
+		Sound_Off(); 
+  }
+    previous = input; 
+    delay(10);  // remove switch bounce   
+  }
+            
+}
+
+// Inputs: Number of msec to delay
+// Outputs: None
+void delay(unsigned long msec){ 
+  unsigned long count;
+  while(msec > 0 ) {  // repeat while there are still delay
+    count = 16000;    // about 1ms
+    while (count > 0) { 
+      count--;
+    } // This while loop takes approximately 3 cycles
+    msec--;
+  }
+}
---
+++ CPE187/Lab Source Code/Lab13/Algador_CpE187_Lab13_Piano.c
@@ -0,0 +1,34 @@
+// Piano.c
+// Runs on LM4F120 or TM4C123, 
+// edX lab 13 
+// There are four keys in the piano
+// Daniel Valvano
+// December 29, 2014
+
+// Port E bits 3-0 have 4 piano keys
+
+#include "Piano.h"
+#include "..//tm4c123gh6pm.h"
+
+
+// **************Piano_Init*********************
+// Initialize piano key inputs
+// Input: none
+// Output: none
+void Piano_Init(void){ 
+  GPIO_PORTE_AMSEL_R &= ~0x0F; // 3) disable analog function on PE2-0
+  GPIO_PORTE_PCTL_R &= ~0x000000FF; // 4) enable regular GPIO
+  GPIO_PORTE_DIR_R &= ~0x0F;   // 5) inputs on PE2-0
+  GPIO_PORTE_AFSEL_R &= ~0x0F; // 6) regular function on PE2-0
+  GPIO_PORTE_DEN_R |= 0x0F;    // 7) enable digital on PE2-0
+}
+// **************Piano_In*********************
+// Input from piano key inputs
+// Input: none 
+// Output: 0 to 15 depending on keys
+// 0x01 is key 0 pressed, 0x02 is key 1 pressed,
+// 0x04 is key 2 pressed, 0x08 is key 3 pressed
+unsigned long Piano_In(void){
+  
+  return (GPIO_PORTE_DATA_R&0x0F); // remove this, replace with input
+}
